97f4057ca07885596ee68bc86b3c521ad8c8f31c
Jordy Moreno Arias <yordimorenoarias.11@gmail.com>
2025-10-28 10:32:30 -0400

    Add project files.

---
+++ ClinicManagerMAUI/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace ClinicManagerMAUI
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
